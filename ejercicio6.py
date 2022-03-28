e7706f12e7c2ce936a7c84d1af98264f8f909c54
FedeLetieri <ftlettieri@estudiantes.unsam.edu.ar>
2022-03-28 17:00:23 -0300

    Falta terminar punto 2

--- ejercicio6.py
+++ ejercicio6.py
@@ -5,6 +5,8 @@
 "para que devuelva por pantalla \n"
 "> __Color: {El color del vehículo}, {Velocidad} Km/h, {Cantidad de ruedas} ruedas, {Cilindraje} cc\".__"
 
+
+"********************************************************************************************************"
 class vehiculos():
     "Metodos"
     def __init__(self,color,ruedas):
@@ -15,6 +17,47 @@ class vehiculos():
     def __str__(self):
         return f"El color del vehiculos es {self.color} y tiene {self.ruedas} ruedas"
 
+
+"********************************************************************************************************"
+"PRIMERA SUBCLASE DE VEHICULO"
+class carro(vehiculos):
+    """ def __init__(self,velocidad,cilindraje):
+        self.velocidad=velocidad
+        self.cilindraje=cilindraje """
+    velocidad=0
+    cilindraje=0
+
+    def __str__(self):
+        return f"Color:{self.color}\n"\
+               f"Velocidad: {self.velocidad} km/h\n"\
+               f"Ruedas:{self.ruedas}\n"\
+               f"Cilindraje:{self.cilindraje} cc\n"    
+"********************************************************************************************************"
+class camioneta(vehiculos):
+    carga=0
+    def __str__(self):
+        return f"Color:{self.color}\n"\
+               f"Ruedas: {self.ruedas} km/h\n"\
+               f"Carga:{self.carga}kg\n"
+
+
+
+
+
+
+
+"Ejecucion:"
+print("Primer Vehiculo:")
 primerAuto=vehiculos("Negro",4)
 print(primerAuto)
-
+"Ejecucion:"
+print("\nSegundo vehiculo:")
+segundoAuto=carro("Gris",4)
+segundoAuto.velocidad=150
+segundoAuto.cilindraje=40
+print(segundoAuto)                
+"Ejecucion:"
+print("\nTercer Vehiculo:")
+tercerAuto=camioneta("Blanco",6)
+camioneta.carga=500
+print(tercerAuto)
