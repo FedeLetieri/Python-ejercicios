2bc86b06d67fe00751a0b39d3e4acd223261c609
FedeLetieri <ftlettieri@estudiantes.unsam.edu.ar>
2022-03-28 16:38:41 -0300

    Punto 1 terminado

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,4 +1,20 @@
-" 1) Crea una superclase llamada Vehículo cuyos atributos sean Color y Ruedas. Redefine el método _str_ para que devuelva por pantalla:\n"
+" 1) Crea una superclase llamada Vehículo cuyos atributos sean Color y Ruedas. Redefine el método _str_"
+"para que devuelva por pantalla:\n"
 "> __Color: {El color del vehículo}, {Cantidad de ruedas} ruedas__. \n"
-"### Crea una subclase llamada Carro y agrega los atributos Velocidad, Cilindraje. Redefine el método _str_ para que devuelva por pantalla \n"
+"### Crea una subclase llamada Carro y agrega los atributos Velocidad, Cilindraje. Redefine el método _str_ "
+"para que devuelva por pantalla \n"
 "> __Color: {El color del vehículo}, {Velocidad} Km/h, {Cantidad de ruedas} ruedas, {Cilindraje} cc\".__"
+
+class vehiculos():
+    "Metodos"
+    def __init__(self,color,ruedas):
+        self.color=color
+        self.ruedas=ruedas
+    
+    "metodo para llamar a la función"
+    def __str__(self):
+        return f"El color del vehiculos es {self.color} y tiene {self.ruedas} ruedas"
+
+primerAuto=vehiculos("Negro",4)
+print(primerAuto)
+
